a0c08fffb1a30501df537a469c05105a39f74752
als79gur49 <alsgur7949@naver.com>
2024-11-22 02:37:18 +0900

    SoundManager 및 BGMManager수정
    SoundManager에서 BGM, Effect에 대한 Toggle기능을 수정
    이전 Volume를 기억 Off ->On하여도 원래 Volume으로 돌아옴
    
    추가로 BGM이 너무 일찍 Play이 되기에 Sound초기화 전에 이루어져서 Sound 설정과 맞지 않게 출력되는 것을 코루틴을 통해 늦게 호출하게 변경

--- Assets/Scripts/Sound/BGMManger.cs
+++ Assets/Scripts/Sound/BGMManger.cs
@@ -16,7 +16,17 @@ public class BGMManger : MonoBehaviour
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {   //씬의 이름과 브금의 key이름이 같을 경우 브금 출력하게 설정
+    {
+        //SoundManager에서 Awake에서 초기화 할 경우 AudioMixer에 반영이 되지 않고,
+        //Start에서 초기화 할 경우 반영은 되지만, 너무 실행되는 지점이 느리기에 초기화 이전에 이미 음악이 실행되기에,
+        //BGM자체를 늦게 호출하게 바꿈
+        StartCoroutine(PlayBGMAfterInit(scene, mode));
+    }
+
+    private IEnumerator PlayBGMAfterInit(Scene scene, LoadSceneMode mode)
+    {
+        yield return null;
+        //씬의 이름과 브금의 key이름이 같을 경우 브금 출력하게 설정
         SoundManager.Instance.PlayBGMAudio(scene.name);
         if (SoundManager.Instance.PlayBGMAudio(scene.name))
         {
