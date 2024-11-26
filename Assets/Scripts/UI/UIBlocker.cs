a7ce48eb37ab7cc552ece38831879d3131f6e3f2
als79gur49 <alsgur7949@naver.com>
2024-11-26 23:42:23 +0900

    UI 기획서에 맞게 수정 및 배치 변경
    UI 기획서에 맞게 크기 조절 및 다양한 변경 - Title, SelectLevel, Stage1 모두 수정함.
    추가로 확인한 대부분의 UI에 LayoutGroup으로 묶어 관리 용이하게 변경함.

---
+++ Assets/Scripts/UI/UIBlocker.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBlocker : MonoBehaviour
+{
+    /// <summary>
+    /// UIBlocker는 stack 가짐, UI창이 켜지면 해당 UI Push, 꺼지면 Pop형태
+    /// UIBlocker는 stack의 Peek의 자식 오브젝트에 위치
+    /// Peek 존재하지 않으면 끄기
+    /// </summary>
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject blocker;
+    private Stack<GameObject> uiStack = new Stack<GameObject>();
+
+    public void OpenUI(GameObject UI)
+    {
+        uiStack.Push(UI);
+
+        ChangeBlockerPosition();
+    }
+    public void CloseUI(GameObject UI)
+    {
+        if(uiStack.Count > 0 && uiStack.Peek() == UI)
+        {
+            uiStack.Pop();
+        }
+
+        ChangeBlockerPosition();
+    }
+
+    private void ChangeBlockerPosition()
+    {
+        if (blocker == null)
+        {
+            blocker = this.gameObject;
+        }
+
+        blocker.SetActive(uiStack.Count > 0); //열려있는 UI가 없을 경우 비활성화
+
+        if(uiStack.TryPeek(out GameObject parentUI)) //제일 위의 UI의 자식으로 Blocker 배치
+        {
+            transform.SetParent(parentUI.transform, true);
+        }
+    }
+}
