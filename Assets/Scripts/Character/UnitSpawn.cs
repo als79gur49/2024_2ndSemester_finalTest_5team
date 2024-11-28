d757ff9c8ee43e68f6e931c98fb46502227784b0
Yujina02 <20221145@vision.hoseo.edu>
2024-11-28 20:45:09 +0900

    몬스터유닛 랜덤 생성
    몬스터 유닛 랜덤생성
    임의로 2초로 설정(추후 변경)

--- Assets/Scripts/Character/UnitSpawn.cs
+++ Assets/Scripts/Character/UnitSpawn.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class UnitSpawn : MonoBehaviour
 {
-    public GameObject[] SpawnUnit;
-
-
-    public GameObject MonsterunitPrefab; // 생성할 적 유닛
+    public GameObject[] SpawnUnit; // 플레이어 유닛 생성
+    public GameObject[] MonsterUnit; // 랜덤 생성 유닛
 
     public Transform PlayerspawnPoint; // 플레이어 생성 위치
     public Transform MonsterspawnPoint; // 몬스터 생성 위치
 
-    public float spawnInterval = 5000f; // 유닛 생성 간격
-    public float spawnTimer; //유닛 생성 딜레이
+    public float spawnDelay = 2f; // 몬스터 생성 간격
 
-    void Start()
+    private float lastSpawnTime; // 마지막으로 몬스터를 생성한 시간
+
+
+    void Update()
     {
-
+        // 시간이 일정 간격 이상 경과했다면 몬스터 생성
+        if (Time.time >= lastSpawnTime + spawnDelay)
+        {
+            SpawnMonster(); // 몬스터 생성
+            lastSpawnTime = Time.time; // 마지막 생성 시간을 현재 시간으로 업데이트
+        }
     }
 
     // 유닛 생성 스폰함수
@@ -34,8 +39,9 @@ public class UnitSpawn : MonoBehaviour
         Instantiate(SpawnUnit[2], PlayerspawnPoint.position, Quaternion.identity); // 궁수 생성
     }
 
-    void spawnMonster()
+    public void SpawnMonster()
     {
-        Instantiate(MonsterunitPrefab, MonsterspawnPoint.position, Quaternion.identity); // 몬스터 생성유닛
+        int randomIndex = Random.Range(0, MonsterUnit.Length);
+        Instantiate(MonsterUnit[randomIndex], MonsterspawnPoint.position, Quaternion.identity); // 몬스터 생성유닛
     }
 }
