7c6dc4a6f1e43224ffd6f557b449779588efb815
als79gur49 <alsgur7949@naver.com>
2024-11-29 20:07:20 +0900

    DataManager분리
    DataManager를 DataManager와 JsonSaveAndLoader로 책임을 분리.
    
    Json에서는 Json에게 읽기 쓰기 역할을 하고
    DataManager는 data저장 및 다른 객체와 읽기 쓰기 역할

--- Assets/Scripts/SaveAndLoad/DataManager.cs
+++ Assets/Scripts/SaveAndLoad/DataManager.cs
@@ -1,6 +1,8 @@
 using System.IO;
 using UnityEngine;
 using System.Text;
+
+[RequireComponent(typeof(JsonSaveAndLoader))]
 public class DataManager : MonoBehaviour
 {
     private static DataManager instance;
@@ -25,14 +27,17 @@ public class DataManager : MonoBehaviour
         }
     }
 
-    private PlayerData playerData;
-    private string dataPath;
 
-    public PlayerData PlayerData
+    private JsonSaveAndLoader saveAndLoader;
+    private PlayerData playerData;
+
+    public PlayerData PlayerData //TODO: 시간 되면 Wrapper클래스처럼 외부 노출 제어해보기
     {
         get { return playerData; }
         set { playerData = value; }
     }
+
+
 
     private void Awake() //싱글턴 패턴
     {
@@ -46,37 +51,19 @@ public class DataManager : MonoBehaviour
         instance = this;
 
         DontDestroyOnLoad(this.gameObject);//싱글턴
-
-        dataPath = Path.Combine(Application.dataPath, "PlayerData.json");
+        
+        saveAndLoader = new JsonSaveAndLoader(); //초기화, 기본 PlayerData.json
         //최초 초기화
         LoadData();
     }
 
     public void LoadData()
     {
-        if(File.Exists(dataPath))
-        {   //FileStream과 달리 자동으로 Close()를 해주기에 편함
-            string json = File.ReadAllText(dataPath); //파일 읽어오기
-
-            playerData = new PlayerData();
-
-            playerData = JsonUtility.FromJson<PlayerData>(json); //읽어온 파일을 객체에 맞게 변환
-            //필요하면 데이터 유효성 검사하기. 최대클리어스테이지와 각 스테이지별 클리어 유무 및 별 획득 개수 등
-
-            Debug.Log(JsonUtility.ToJson(playerData));
-        }
-        else
-        {
-            playerData = new PlayerData();
-            Debug.Log($"{playerData}이 존재하지 않아 {JsonUtility.ToJson(playerData)}초기화 후 {dataPath}에 생성하였습니다.");
-        }
+        playerData = saveAndLoader.LoadData();
     }
 
     public void SaveData()
     {
-        string json = JsonUtility.ToJson(playerData);
-        //(기존 파일 삭제 후)새 파일 생성 후 데이터 저장 후 자동으로 Close()
-        File.WriteAllText(dataPath, json);
-        Debug.Log($"{json}내용이 {dataPath}에 저장되었습니다.");
+        saveAndLoader.SaveData(playerData);
     }
 }
